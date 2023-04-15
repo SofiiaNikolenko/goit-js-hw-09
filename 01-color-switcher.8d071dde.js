09babae911bea5badd21bc1b8cff8d2b6bf0c402
SofiiaNikolenko <117819720+SofiiaNikolenko@users.noreply.github.com>
2023-04-15 20:15:44 +0000

    Deploying to gh-pages from @ SofiiaNikolenko/goit-js-hw-09@a5bab79a4f7d177d1173d4fe2db15016c0f37ddf 🚀

--- 01-color-switcher.8d071dde.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let n;t.addEventListener("click",(function(){n=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){clearInterval(n)}));
-//# sourceMappingURL=01-color-switcher.8d071dde.js.map
---
+++ 01-color-switcher.b341a24b.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("button[data-start]"),a=document.querySelector("button[data-stop]"),d=document.querySelector("body");e.addEventListener("click",(function(){t=setInterval((function(){d.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),e.disabled=!0,a.disabled=!1})),a.addEventListener("click",(function(){clearInterval(t),e.disabled=!1,a.disabled=!0})),a.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.b341a24b.js.map
